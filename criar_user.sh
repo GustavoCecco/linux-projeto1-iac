5c595dc994ef8ee7296b73e68819b336bee8a9c4
GustavoCecco <gustavo.cecco@yahoo.com.br>
2023-04-21 21:49:29 +0000

    Arquivos IaC v.1

---
+++ criar_user.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Criando usuarios do sistema..."
+
+useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
+passwd guest10 -e
+
+useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
+passwd guest11 -e
+
+useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
+passwd guest12 -e
+
+useradd guest13 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
+passwd guest13 -e
+
+echo "Finalizado"
+
+
+
